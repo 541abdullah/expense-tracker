a4bcc78350194d947bf076d019e5db342b219967
Abdullah Awais <164062766+541abdullah@users.noreply.github.com>
2024-07-12 20:04:15 +0530

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
     "preflightContinue": false,
     "optionsSuccessStatus": 204
   })
-  await app.listen(process.env.PORT,'0.0.0.0');
+  await app.listen(4000);
 }
 bootstrap();
